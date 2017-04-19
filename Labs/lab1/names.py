657219bd21d9851dbecbf43937d8549b044bc945
Tawfic Rabbani <trabbani@ucsd.edu>
2017-04-18 17:11:36 -0700

    Added lab 2 folder and instructions

--- Labs/lab1/names.py
+++
@@ -1,4 +0,0 @@
-names = ["Aram", "Tawfic"] #create names list
-
-for i in range(len(names)): #for loop used to iterate through names
-  print "Hello,", names[i] #print statement printing Hello, <name>
